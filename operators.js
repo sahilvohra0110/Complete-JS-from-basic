58f6c2ddae4abe186b9aac5db0819be4e902199a
Sahil Vohra <112792843+sahilvohra0110@users.noreply.github.com>
2023-02-18 16:34:46 +0530

    Type of operators with defination

---
+++ operators.js
@@ -0,0 +1,47 @@
+// var a= 1 //1 is operand
+// var b= 2 //2 is operand
+// var c= a+b // c is result and + is operator
+
+// // console.log(c) // here 1 and 2 are operands, '+' is operator and , c is result
+
+
+// //operators: Arithmetic operators-->
+
+var x= 20
+var y= 2
+// console.log(x+y)
+// console.log(x-y)
+// console.log(x*y)
+// console.log(x/y)
+// console.log(x**y)
+// console.log(x%y)
+// console.log(++x)
+// console.log(y--)
+
+
+//assignment operators-->
+
+var n = 10
+var m = 20
+console.log(n=m)
+console.log(n**m)
+
+//comparison operator-->
+
+let num1=10
+let num2=30
+let num3="5"
+
+// console.log(num1==num2)
+// console.log(num1!=num2)
+// console.log(num1===num3) //to check the data type
+// console.log(num1!==num3)
+
+//logical operators-->
+
+let log1=10
+let log2=20
+console.log(log1>log2 && log1==log2) // if any of the situation if false, result will be false.
+console.log(log1<log2 || log1==log2) // if any of the situation if true, result will be true. &&--> and, ||--> or.
+
+

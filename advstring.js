7cecf8bc4982c19df6472c511c1586b2a667b722
Sahil Vohra <112792843+sahilvohra0110@users.noreply.github.com>
2023-02-27 00:30:28 +0530

    Advance method in strings

---
+++ advstring.js
@@ -0,0 +1,7 @@
+let name= 'sahil'
+console.log(name.toUpperCase());//result-SAHIL
+console.log(name.toLowerCase()); //result-sahil
+console.log(name.slice(2,4)); //result- 2nd and 4th string
+
+let newName= name.replace('sahil','vohra');
+console.log(newName);

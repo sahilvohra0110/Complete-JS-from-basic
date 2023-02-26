ebce15b8d2a9af4ce7fb977c1833102c40358c89
Sahil Vohra <112792843+sahilvohra0110@users.noreply.github.com>
2023-02-27 00:18:31 +0530

    What is strings

---
+++ basicstrings.js
@@ -0,0 +1,12 @@
+let name ="sahil"
+console.log(name.length);
+
+console.log(name[3]); //to open string by strging-> s-0, a-1, h-2, i-3, l-4
+
+//template literal->
+
+let boy1 = 'sahil'
+let boy2 = 'nitin'
+
+let sentence = `${boy1} is a friend of ${boy2}`
+console.log(sentence);

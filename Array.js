8e368e558fc798728199dcc50a6bec4293e5179c
Sahil Vohra <112792843+sahilvohra0110@users.noreply.github.com>
2023-02-27 23:01:59 +0530

    basic array

---
+++ Array.js
@@ -0,0 +1,16 @@
+let marks= [20,30,40,50,60] //it's how array look like
+
+console.log(marks[2]);// to access the array
+
+marks[5]=0 //to add value in array
+
+console.log(marks.length-1); //to check lenght of arr
+console.log(marks[5]);
+console.log(marks[5-3]);
+console.log(marks);
+console.log(typeof marks);
+
+
+for(let i=0;i<marks.length;i++){
+    console.log(marks[i]);
+}

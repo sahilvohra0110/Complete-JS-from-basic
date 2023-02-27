5d36e34d9b03b96598d677c6729617c034724267
Sahil Vohra <112792843+sahilvohra0110@users.noreply.github.com>
2023-02-27 23:02:11 +0530

    method of array

---
+++ arrmethod.js
@@ -0,0 +1,15 @@
+//to convert arr into strings.
+
+let arr = [1,2,3,4,5,6]
+let n= arr.toString()
+console.log(n);
+
+// pop arr
+
+let arr1= [1,2,3,4,5]
+
+arr1.pop() //to pop the last value
+let p=arr1.pop() // to print the popped value
+console.log(arr1);
+console.log(p);
+
